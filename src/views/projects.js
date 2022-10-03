5a7a1e6a54d457ad6481b5743d068a296f1bb273
Sol <solemich@gmail.com>
2022-10-03 10:57:29 -0300

    grid projects

---
+++ src/views/projects.js
@@ -0,0 +1,22 @@
+const Projects = () => {
+    
+    const template = //html
+    `
+    <div id="root">
+      <div class="container-projects">
+        <div class="proy1"></div>
+        <div class="proy2"></div>
+        <div class="proy3"></div>
+        <div class="proy4"></div>
+        <div class="proy5"></div>
+        <div class="proy6"></div>
+      </div>
+        
+        
+    </div>
+    
+    
+    `
+    
+
+} 
