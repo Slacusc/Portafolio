3ac91398ba28abcab4909220cfbe191414c06372
Sol <solemich@gmail.com>
2022-10-03 22:18:24 -0300

    first carrousel

--- src/views/projects.js
+++ src/views/projects.js
@@ -7,8 +7,8 @@ const Projects = () => {
     <div class="proy1"></div>
     <div class="proy2"></div>
     <div class="proy3">
-    <img class="icon3"  src="./src/img/icongit.png" alt="github image">
-    <img class="icon2"  src="./src/img/iconlink.png" alt="linkedin image">
+    <img class="icon4"  src="./src/img/icongit.png" alt="github image">
+    <img class="icon5"  src="./src/img/iconlink.png" alt="linkedin image">
     </div>
     <div class="proy4"></div>
     <div class="proy5"></div>
@@ -16,7 +16,16 @@ const Projects = () => {
       <h1 class="title-project">Proyectos </h1>
     </div>
     <div class="proy7"></div>
-    <div class="proy8"></div>
+    <div class="proy8">
+      <div class="slider-container">
+        <img class="slider-item" src="./src/img/st.png" alt="coding game">
+        <img class="slider-item" src="./src/img/pg.png" alt="pokeGoogle">
+        <img class="slider-item" src="./src/img/ff.png" alt="Fancosfic">
+        <img class="slider-item" src="./src/img/st.png" alt="coding game">
+        <img class="slider-item" src="./src/img/pg.png" alt="pokeGoogle">
+        <img class="slider-item" src="./src/img/ff.png" alt="Fancosfic">
+      </div>  
+    </div>
   </div>
         
     </div>
