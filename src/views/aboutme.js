a6c92c2692de68f1547cbe0b19564754474ff432
Sol <solemich@gmail.com>
2022-10-05 00:21:46 -0300

    carrousel with flip and one card

--- src/views/aboutme.js
+++ src/views/aboutme.js
@@ -12,13 +12,22 @@ const About = () => {
         <div class="about3"></div>
         <div class="about4">
         <h1 class="name3">Sobre mí</h1>
-        <h3 class="bio">Tengo más de 10 años de experiencia en diseño de la interfaz de usuario y la experiencia de usuario de decenas de sitios web y aplicaciones principales de hoy en día. 
-        Me apasiona el diseño atrevido pero intuitivo y trabajar para empresas con responsabilidad social.
-        Tengo más de 10 años de experiencia en diseño de la interfaz de usuario y la experiencia de usuario de decenas de sitios web y aplicaciones principales de hoy en día. 
-        Me apasiona el diseño atrevido pero intuitivo y trabajar para empresas con responsabilidad social.
-        
-        Tengo más de 10 años de experiencia en diseño de la interfaz de usuario y la experiencia de usuario de decenas de sitios web y aplicaciones principales de hoy en día. 
-        Me apasiona el diseño atrevido pero intuitivo y trabajar para empresas con responsabilidad social.</h3></div>
+        <h3 class="bio">Hola! Mi nombre es Soledad y realmente disfruto crear, diseñar e inventar soluciones atractivas visualmente. Además soy escritora de Ciencia Ficción y lidero una agrupación 
+        de mujeres escritoras llamada "La Ventana del Sur" en donde visibilizamos a escritoras que escriben en los tres géneros: Ciencia Ficción, Fantasía y Terror</h3>
+        <br>
+        <h3 class="bio">Mi interés por la programación se remonta desde que tenía 14 años, en ese tiempo aprendí a diseñar páginas web ocupando Dreamweaver y aplicar algo de código en HTML y CSS!</h3>
+        <br>
+        <h3 class="bio">A pesar de que no pude seguir con una carrera en programación, siempre guardé el anhelo de volver a ello. La vida da muchas vueltas y ese sueño no se concretó en ese tiempo.
+        Hoy a mis 36 años por fin puedo decir orgullosamente que soy Desarrolladora Frontend. Recientemente he estado trabajando en portales web relacionados con la literatura para buscar una interacción usuario mucho 
+        más atractiva y llamativa.</h3>
+        <br>
+        <h3 class="bio"> Algunas de las herramientas que he ocupado son:</h3>
+        <li class="bio">Javascript</li>
+        <li class="bio">CSS</li>
+        <li class="bio">HTML</li>
+        <li class="bio">React</li>
+
+        </div>
         <div class="about5"></div>
         <div class="about6">
         <img class="bio-photo" src="./src/img/biofoto.png" alt="biophoto">
--- src/views/projects.js
+++ src/views/projects.js
@@ -14,14 +14,22 @@ const Projects = () => {
     <div class="proy5"></div>
     <div class="proy6">
       <h1 class="title-project">Proyectos </h1>
+      <h3 class="text-project">Tengo más de 10 años de experiencia en diseño de la interfaz de usuario y la experiencia de usuario de decenas de sitios web y aplicaciones principales de hoy en día. 
+      Me apasiona el diseño atrevido pero intuitivo y trabajar para empresas con responsabilidad social.
+      Tengo más de 10 años de experiencia en diseño de la interfaz de usuario y la experiencia de usuario de decenas de sitios web y aplicaciones principales de hoy en día. 
+      Me apasiona el diseño atrevido pero intuitivo y trabajar para empresas con responsabilidad social.</h3>
     </div>
     <div class="proy7"></div>
     <div class="proy8">
      <div class="slider">
       <div class="slider-container">
         <div  class="slide">
-        <a href="https://slacusc.github.io/Survivors-Stage-Cipher/" target="_blank">
-          <img src="./src/img/st.png" alt="coding game">
+          <div class="front">
+            <img src="./src/img/st.png" alt="coding game">
+          </div>
+          <div class="back">
+          <img src="./src/img/card1.png" alt="card1">
+          </div>
         </div>
         <div  class="slide">
         <a href="https://github.com/Slacusc/Fancosfic" target="_blank">
