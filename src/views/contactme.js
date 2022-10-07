6c700ed6ab00384af03ea97862b33abdf5f633ab
Sol <solemich@gmail.com>
2022-10-07 00:39:51 -0300

    Portafolio

--- src/views/contactme.js
+++ src/views/contactme.js
@@ -9,10 +9,19 @@ const Contact = () => {
                 <h1 class="name4">Contáctame</h1>
             </div>
             <div class="contact3"></div>
-            <div class="contac4">
-                <div class="ver-line3">
+            <div class="contact4">
+                <div class="ver-line3"></div>
             </div>
-            <div class="contact5"></div>
+            <div class="contact5">
+                <a href="https://www.linkedin.com/in/soledad-cort%C3%A9s-473a27228/" class="links" style="text-decoration-line:none" Target="_blank"><i class="fa-brands fa-linkedin"></i>
+                Linkedin</a>
+                <br>
+                <a href="https://github.com/Slacusc" class="links" style="text-decoration-line:none" Target="_blank"><i class="fa-brands fa-github"></i>
+                GitHub</a>
+
+
+            
+            </div>
             <div class="contact6"></div>
         </div>
     </div>    
